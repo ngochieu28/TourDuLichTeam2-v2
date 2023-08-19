3a8b9d9f3452d3d57f0997c7e7d9823f69a8db36
ngochieu28 <nguyenngochieu28201@gmail.com>
2023-08-19 18:43:24 +0700

    Update srcImg.js

--- frontend/src/util/srcImg.js
+++ frontend/src/util/srcImg.js
@@ -2,7 +2,7 @@
 
 export const srcImg = (() => {
   const src1 = "http://192.168.1.163:4000";
-  const src2 = "http://192.168.0.101";
+  const src2 = "http://192.168.0.101:4000";
 
   return src1 ? src1 : src2;
 })();
