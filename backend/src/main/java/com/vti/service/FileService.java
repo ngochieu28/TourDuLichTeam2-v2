d1191f89678c05cbac0f25fdc701a000c506f54d
ngochieu28 <nguyenngochieu28201@gmail.com>
2023-08-31 00:29:47 +0700

    Update FileService.java

--- backend/src/main/java/com/vti/service/FileService.java
+++ backend/src/main/java/com/vti/service/FileService.java
@@ -18,26 +18,19 @@ public class FileService implements IFileService {
 	private FileManager fileManager = new FileManager();
 	private String linkFolder = "/home/ubuntu/TourDuLichTeam2-v2/image";
 
-
 	@Override
 	public String uploadImage(MultipartFile image) throws IOException {
-
 		String nameImage = new Date().getTime() + "." + fileManager.getFormatFile(image.getOriginalFilename());
-
-		String path = linkFolder + "\\" + nameImage;
-
+		String path = linkFolder + "/" + nameImage;
 		fileManager.createNewMultiPartFile(path, image);
-
 		// TODO save link file to database
-
 		// return link uploaded file
 		return nameImage;
 	}
 
 	@Override
 	public byte[] getImage(String fileName) throws IOException {
-		String filePath = linkFolder + "\\" + fileName;
-
+		String filePath = linkFolder + "/" + fileName;
 		// Đọc dữ liệu của file ảnh thành mảng byte[]
 		Path imagePath = Paths.get(filePath);
 		return Files.readAllBytes(imagePath);
