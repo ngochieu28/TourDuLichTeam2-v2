cf00c731e41d7a6240ef36c5e9d0fc879b776f0d
ngochieu28 <nguyenngochieu28201@gmail.com>
2023-09-11 10:15:10 +0700

    swagger

--- frontend/src/util/srcImg.js
+++ frontend/src/util/srcImg.js
@@ -1,7 +1,8 @@
 // constants.js
 
 export const srcImg = (() => {
-  const src1 = "http://13.212.56.39:8080/api/v1/files/image";
+  const src1 = "http://localhost:8080/api/v1/files/image";
+  // const src1 = "http://18.141.217.112:8080/api/v1/files/image";
   const src2 = "http://192.168.0.101:4000";
 
   return src1 ? src1 : src2;
